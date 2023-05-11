7ed0a146f7eb4ea2f90fb4547b32c9eed22e7afc
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2023-05-11 15:11:09 -0300

    refactor.added the software arquitecture

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require("express");
+const userRoute = require("./src/routes/user.route")
+
 const app = express();
 
-//ROTA
-    //Method HTTP - CRUD (CREATE, READ, UPDATE, DELETE)
-        //GET - Pega uma info
-        //POST - Cria uma info
-        //PUT - Altera uma info
-        //PATH - Altera parte de uma info
-        //DELETE - Apaga uma info
+app.use("/soma", userRoute);
 
-    //Name - Um identificador da rota
 
-    //Function (Callback) -Responsavel por executar algum comando
 
-app.get("/home", (req,res) => {
-   const soma = 1 + 2;
-   
-   res.json(soma)
-});
 
 app.listen(3000);
