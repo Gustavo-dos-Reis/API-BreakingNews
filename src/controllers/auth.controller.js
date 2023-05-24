a1a41194dab203564c33b00e4f3c54d4a9b2b34e
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2023-05-24 19:27:27 -0300

    added the JWT

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt';
-import { loginService } from '../services/auth.service.js';
+import { loginService, generateToken } from '../services/auth.service.js';
 
 
 const login = async (req, res) => {
@@ -19,7 +19,9 @@ const login = async (req, res) => {
 
        console.log(passwordIsValid)
 
-        res.send("Login ok")
+       const token = generateToken(user.id, user.email)
+
+        res.send({token})
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,5 +1,8 @@
 import User from '../models/User.js';
+import jwt from 'jsonwebtoken'
 
 const loginService = (email) => User.findOne({email: email}).select("+password");
 
-export { loginService };
+const generateToken = (id, email) => jwt.sign({id: id, email: email}, process.env.SECRET_JWT, {expiresIn: 86400});
+
+export { loginService, generateToken };
