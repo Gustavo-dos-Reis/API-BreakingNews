d21c8b46ee6e10d8328fc8a3434da82e47a4f7a1
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2023-06-07 15:39:20 -0300

    I created the limit and offset parameters

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js"
+import { createService, findAllService, countNews } from "../services/news.service.js"
 
 const create = async (req, res) => {
    try{ 
@@ -24,14 +24,54 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req,res) => {
-    const news = await findAllService();
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+        limit = 5;
+    }
+
+    if (!offset) {
+        offset = 0;
+    }
+    
+    const news = await findAllService(offset, limit);
+    const total = await countNews();
+    const currentnUrl = req.baseUrl;
+    console.log(currentnUrl)
+
+    const next = offset + limit;
+    const nextUrl = next < total ? `${currentnUrl}?limit=${limit}&offset=${next}`: null;
+
+    const previus = offset - limit < 0 ? null : offset - limit;
+    const previusUrl = previus != null ?`${currentnUrl}?limit=${limit}&offset=${previus}`: null;
 
     if (news.length === 0) {
         return res.status(400).send({ 
             message: "There are no registered news",
         })
     }
-    res.send(news);
+    res.send({
+        nextUrl,
+        previusUrl,
+        limit,
+        offset,
+        total,
+
+        results: news.map(item => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.benner,
+            likes: item.likes,
+            coments: item.coments,
+            name: item.user.name,
+            username: item.user.name,
+            userAvatar: item.user.avatar
+        }))
+    });
 };
 
-export  { create, findAll};
+export  { create, findAll };
