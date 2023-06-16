c6dfc73db0af575db9ae30cd7459ce71a477eb22
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2023-06-16 19:34:01 -0300

    Fetching the first data from a list

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,9 @@
-import { createService, findAllService, countNews } from "../services/news.service.js"
+import { 
+    createService, 
+    findAllService, 
+    countNews,
+    topNewsService
+} from "../services/news.service.js"
 
 const create = async (req, res) => {
    try{ 
@@ -24,54 +29,84 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req,res) => {
-    let { limit, offset } = req.query;
+    try {
+        let { limit, offset } = req.query;
 
-    limit = Number(limit);
-    offset = Number(offset);
+        limit = Number(limit);
+        offset = Number(offset);
 
-    if (!limit) {
-        limit = 5;
+        if (!limit) {
+            limit = 5;
     }
 
-    if (!offset) {
-        offset = 0;
-    }
+        if (!offset) {
+            offset = 0;
+        }
     
-    const news = await findAllService(offset, limit);
-    const total = await countNews();
-    const currentnUrl = req.baseUrl;
-    console.log(currentnUrl)
+        const news = await findAllService(offset, limit);
+        const total = await countNews();
+        const currentnUrl = req.baseUrl;
+        console.log(currentnUrl)
 
-    const next = offset + limit;
-    const nextUrl = next < total ? `${currentnUrl}?limit=${limit}&offset=${next}`: null;
+        const next = offset + limit;
+        const nextUrl = next < total ? `${currentnUrl}?limit=$  {limit}&offset=${next}`: null;
 
-    const previus = offset - limit < 0 ? null : offset - limit;
-    const previusUrl = previus != null ?`${currentnUrl}?limit=${limit}&offset=${previus}`: null;
+        const previus = offset - limit < 0 ? null : offset - limit;
+        const previusUrl = previus != null ?`${currentnUrl}?limit=$ {limit}&offset=${previus}`: null;
 
-    if (news.length === 0) {
-        return res.status(400).send({ 
-            message: "There are no registered news",
-        })
-    }
-    res.send({
-        nextUrl,
-        previusUrl,
-        limit,
-        offset,
-        total,
+        if (news.length === 0) {
+            return res.status(400).send({ 
+                message: "There are no registered news",
+            })
+        }
+        res.send({
+            nextUrl,
+            previusUrl,
+            limit,
+            offset,
+            total,
 
-        results: news.map(item => ({
-            id: item._id,
-            title: item.title,
-            text: item.text,
-            banner: item.benner,
-            likes: item.likes,
-            coments: item.coments,
-            name: item.user.name,
-            username: item.user.name,
-            userAvatar: item.user.avatar
-        }))
-    });
+            results: news.map(item => ({
+                id: item._id,
+                title: item.title,
+                text: item.text,
+                banner: item.banner,
+                likes: item.likes,
+                coments: item.comments,
+                name: item.user.name,
+                username: item.user.name,
+                userAvatar: item.user.avatar
+            }))
+        });
+    } catch (err) {
+        res.status(500).send(err.message);
+      } 
+
+    
 };
 
-export  { create, findAll };
+const topNews = async (req,res) =>  {
+    try{
+        const news = await topNewsService();
+
+        if(!news) {return res.status(400).send({message: "There are no registered news"})};
+
+        res.send({
+            news:{
+                id: news._id,
+                title: news.title,
+                text: news.text,
+                banner: news.banner,
+                likes: news.likes,
+                comments: news.comments,
+                name: news.user.name,
+                username: news.user.name,
+                userAvatar: news.user.avatar
+            }
+        })
+    } catch (err) {
+      res.status(500).send(err.message);
+    } 
+};
+
+export  { create, findAll, topNews };
