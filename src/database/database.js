859a1e202534344ab583903f456d57539134bac4
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2024-08-21 19:44:35 -0300

    refatorei o arquivo database.js

--- src/database/database.js
+++ src/database/database.js
@@ -1,17 +1,12 @@
 import mongoose from 'mongoose';
 
-const conetcDatabase = () => {
-    console.log("Wait connecting to the database...")
+const connectDatabase = () => {
+    console.log("Wait connecting to the database...");
 
     mongoose
-        .connect(process.env.MONGODB_URI ,
-            {
-                useNewUrlParser: true ,
-                useUnifiedTopology: true
-            }
-    )
-    .then(() => console.log("Mongo DB Atlas Connected"))
-    .catch((err) => console.log(`Error connecting to MongoDB Atlas: ${err}`));
+        .connect(process.env.MONGODB_URI)
+        .then(() => console.log("MongoDB Atlas Connected"))
+        .catch((err) => console.log(`Error connecting to MongoDB Atlas: ${err}`));
 };
 
-export default conetcDatabase;
+export default connectDatabase;
