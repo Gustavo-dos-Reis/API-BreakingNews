228f71c3b8c7bceb300fbaa0e75ccb654b9f54b7
Gustavo dos Reis <121633772+Gustavo-dos-Reis@users.noreply.github.com>
2025-01-10 19:22:36 -0300

    bug fixed

--- src/routes/swagger.route.cjs
+++ src/routes/swagger.route.cjs
@@ -4,6 +4,6 @@ const swaggerUi = require("swagger-ui-express")
 const swaggerRouter = require("../swagger.json");
 
 router.use("/", swaggerUi.serve);
-router.get("/", swaggerUi.setup(swaggerDocument));
+router.get("/", swaggerUi.setup(swaggerRouter));
 
 module.exports = router;
